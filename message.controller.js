48317ee797e45dff11fa09753dadc2e83bf10385
darylshy <darylshy@yahoo.com>
2017-02-05 01:12:52 -0500

    example from page 113 of ng-book

--- message.controller.js
+++ message.controller.js
@@ -1,37 +1,16 @@
 (function(){
+
     let app = angular.module('app');
 
-    app.controller('GuessingGameCtrl', function ($scope) {
-
-           $scope.correct = Math.floor(Math.random() * 1000);
-           $scope.currentguess = null;
-           $scope.message = "";
-           $scope.$watch('currentguess', function (newValue, oldValue) {
-               $scope.previousguess = oldValue;
-           });
-
-
-       $scope.colderWarmer = function (guess) {
-           $scope.currentguess = guess;
-
-           if($scope.currentguess === $scope.correct){
-               $scope.message = "Correct!";
-               return;
-           }
-
-           if($scope.currentguess && $scope.previousguess){
-               if(Math.abs($scope.currentguess - $scope.correct) > Math.abs($scope.previousguess - $scope.correct)){
-                   $scope.message =  "Getting Colder";
-               }else if(Math.abs($scope.currentguess - $scope.correct) < Math.abs($scope.previousguess - $scope.correct)){
-                   $scope.message =  "Getting Warmer";
-               }else{
-                   $scope.message =  "Lukewarm";
-               }
-           }else{
-               $scope.message =  "Lukewarm";
-           }
-       };
+    app.controller('SomeController', function () {
 
     });
+
+    app.controller('SecondController', function () {
+
+    });
+
+
+
 
 })();
--- message.directive.js
+++ message.directive.js
@@ -2,24 +2,10 @@
 
     let app = angular.module('app');
 
-    app.directive('guessingGameTitle', function () {
-        return {
-            restrict: 'E',
-            templateUrl: `message.template.htm`,
-            replace: true,
-            transclude: true
-        }
-    });
-
-
-    app.directive('alertDanger', function () {
+    app.directive('myDirective', function() {
         return {
             restrict: 'A',
-            link: function (scope, elem, attrs) {
-                elem.on('click', function () {
-                    alert(scope.previousguess);
-                });
-            }
+            scope: true
         };
     });
 
