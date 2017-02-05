e6671a05de1279c376ca4c62f19968162dfe72a5
darylshy <darylshy@yahoo.com>
2017-02-05 04:20:09 -0500

    Understanding custom directive scope

--- pojs.js
+++ pojs.js
@@ -1,25 +1,68 @@
 /**
  * Created by darylshy on 2/5/17.
  * The purpose of this program is to illustrate the difference between
- * controller scope and directive scope
+ * custom directive's -- scope: true/false
+ */
+class Controller{
+    constructor(scope){
+        let vm = this;
+        vm.name = "Daryl Shy";
+        vm.message = "Hello";
+        vm.scope = scope;
+    }
+    sayHello(){
+        return `${this.message} ${this.name}`;
+    }
+}
+
+let someController = new Controller({});
+someController.someDirective = {};
+//Output of someDirective
+/*
+Controller
+ message:"Hello"
+ name:"Daryl Shy"
+ scope:Object
+ someDirective:Object <---- The directive is on the controller and doesn't have its own scope
+ __proto__:Object
+*/
+
+let someOtherDirective =  new Controller({});
+/*
+someOtherDirective <--- The directive inherits from the controller and has its own scope
+ message:"Hello"
+ name:"Daryl Shy"
+ scope:Object <----own scope
+*/
+console.log(someController);
+console.log(someOtherDirective);
+
+
+/**
+ * In Angular, when we create a new directive, we can choose to set the scope to true or false
+ * =============================================================================================
+ * It is set to false by default. This is equivalent to someDirective above, where the directive is
+ * placed on the controller and does not have its own scope
+ *
+ * Setting it to true is equivalent to someOtherDirective above. It uses prototypical inheritance
+ * and has its own scope.
  */
 
-//This is basically how controller-scope in angular works
-//Controller scopes are basically closures. They are NOT prototypical inheritance.
-//Nested controllers, by default, have access to the outer closure scope through javascript scope-chain. This is just plain old javascript..Nothing new or special...
-function globalScope() {
-    let scope = {};
-    scope.globalSettings = "Some global settings";
-    return function parentScope() {
-         scope.parentScope = {};
-         scope.parentScope.parentSettings = "Some parent settings";
-        return function childScope() {
-            scope.parentScope.childScope = {};
-            scope.parentScope.childScope.someValue = "Some Value from Child Scope";
-            return scope;
-        };
-    };
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
