36302c31cdef1faf8b983e99ad2efec0168303e7
darylshy <darylshy@yahoo.com>
2017-02-05 03:47:59 -0500

    created a controller as an es6 class

--- message.controller.js
+++ message.controller.js
@@ -2,15 +2,19 @@
 
     let app = angular.module('app');
 
-    app.controller('SomeController', function () {
+    class MyController{
+        constructor($scope){
+            let vm = this;
+            vm.name = "Daryl Shy";
+            vm.message = "Hello"
+        }
+        sayHello(){
+            return `${this.message} ${this.name}`;
+        }
+    }
 
-    });
+   app.controller('ClassController', MyController);
 
-    app.controller('SecondController', function () {
-
-    });
-
-
-
+   MyController.$inject = ['$scope'];
 
 })();
