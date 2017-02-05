6be2d769d0c11b25d24c88e90b7a005b28fe3e6a
darylshy <darylshy@yahoo.com>
2017-02-04 23:44:57 -0500

    saving changes so i can work on something else

--- message.controller.js
+++ message.controller.js
@@ -5,8 +5,10 @@
 
            $scope.correct = Math.floor(Math.random() * 1000);
            $scope.currentguess = null;
-           $scope.previousguess = null;
-           $scope.message = ""
+           $scope.message = "";
+           $scope.$watch('currentguess', function (newValue, oldValue) {
+               $scope.previousguess = oldValue;
+           });
 
 
        $scope.colderWarmer = function (guess) {
@@ -19,17 +21,13 @@
 
            if($scope.currentguess && $scope.previousguess){
                if(Math.abs($scope.currentguess - $scope.correct) > Math.abs($scope.previousguess - $scope.correct)){
-                   $scope.previousguess = $scope.currentguess;
                    $scope.message =  "Getting Colder";
                }else if(Math.abs($scope.currentguess - $scope.correct) < Math.abs($scope.previousguess - $scope.correct)){
-                   $scope.previousguess = $scope.currentguess;
                    $scope.message =  "Getting Warmer";
                }else{
-                   $scope.previousguess = $scope.currentguess;
                    $scope.message =  "Lukewarm";
                }
            }else{
-               $scope.previousguess = $scope.currentguess;
                $scope.message =  "Lukewarm";
            }
        };
--- message.directive.js
+++ message.directive.js
@@ -2,12 +2,25 @@
 
     let app = angular.module('app');
 
-    app.directive('messageReply', function () {
+    app.directive('guessingGameTitle', function () {
         return {
             restrict: 'E',
             templateUrl: `message.template.htm`,
-            replace: true
+            replace: true,
+            transclude: true
         }
+    });
+
+
+    app.directive('alertDanger', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, elem, attrs) {
+                elem.on('click', function () {
+                    alert(scope.previousguess);
+                });
+            }
+        };
     });
 
 })();
